63fe4b27445acec6022a86d685af3d1045941e21
Soifou <fleur.fr@gmail.com>
2026-03-13 15:53:24 +0100

    refactor: normalize api actions to return boolean

--- lua/blink/cmp/init.lua
+++ lua/blink/cmp/init.lua
@@ -59,12 +59,13 @@ function cmp.is_documentation_visible() return require('blink.cmp.completion.win
 
 --- Show the completion window
 --- @param opts? blink.cmp.ShowOpts
+--- @return boolean
 function cmp.show(opts)
   opts = opts or {}
 
   -- TODO: when passed a list of providers, we should check if we're already showing the menu
   -- with that list of providers
-  if require('blink.cmp.completion.windows.menu').win:is_open() and not (opts and opts.providers) then return end
+  if require('blink.cmp.completion.windows.menu').win:is_open() and not (opts and opts.providers) then return false end
 
   require('blink.cmp.completion.windows.menu').force_auto_show()
 
@@ -93,14 +94,17 @@ end
 
 -- Show the completion window and select the first item
 --- @params opts? { providers?: string[], callback?: fun() }
+--- @return boolean
 function cmp.show_and_insert(opts)
   opts = opts or {}
   opts.initial_selected_item_idx = opts.initial_selected_item_idx or 1
+
   return cmp.show(opts)
 end
 
 --- Select the first completion item if there are multiple candidates, or accept it if there is only one, after showing
 --- @param opts? blink.cmp.ShowOpts
+--- @return boolean
 function cmp.show_and_insert_or_accept_single(opts)
   local list = require('blink.cmp.completion.list')
   opts = opts or {}
@@ -120,13 +124,15 @@ function cmp.show_and_insert_or_accept_single(opts)
       callback()
     end
   end
+
   return cmp.show(opts)
 end
 
 --- Hide the completion window
 --- @param opts? { callback?: fun() }
+--- @return boolean
 function cmp.hide(opts)
-  if not cmp.is_visible() then return end
+  if not cmp.is_visible() then return false end
 
   require('blink.cmp.completion.trigger').hide()
   if opts and opts.callback then opts.callback() end
@@ -135,23 +141,27 @@ end
 
 --- Cancel the current completion, undoing the preview from auto_insert
 --- @param opts? { callback?: fun() }
+--- @return boolean
 function cmp.cancel(opts)
-  if not cmp.is_visible() then return end
+  if not cmp.is_visible() then return false end
+
   require('blink.cmp.completion.list').undo_preview()
   require('blink.cmp.completion.trigger').hide()
   if opts and opts.callback then opts.callback() end
+
   return true
 end
 
 --- Accept the current completion item
 --- @param opts? blink.cmp.CompletionListAcceptOpts
+--- @return boolean
 function cmp.accept(opts)
   opts = opts or {}
-  if not cmp.is_visible() and not opts.force then return end
+  if not cmp.is_visible() and not opts.force then return false end
 
   local completion_list = require('blink.cmp.completion.list')
   local item = opts.index ~= nil and completion_list.items[opts.index] or completion_list.get_selected_item()
-  if item == nil then return end
+  if item == nil then return false end
 
   completion_list.accept(opts)
   return true
@@ -159,20 +169,23 @@ end
 
 --- Select the first completion item, if there's no selection, and accept
 --- @param opts? blink.cmp.CompletionListSelectAndAcceptOpts
+--- @return boolean
 function cmp.select_and_accept(opts)
   opts = opts or {}
-  if not cmp.is_visible() and not opts.force then return end
+  if not cmp.is_visible() and not opts.force then return false end
 
   local completion_list = require('blink.cmp.completion.list')
   completion_list.accept({
     index = completion_list.selected_item_idx or 1,
     callback = opts.callback,
   })
+
   return true
 end
 
 --- Accept the current completion item and feed an enter key to neovim (e.g. to execute the current command in cmdline mode)
 --- @param opts? blink.cmp.CompletionListSelectAndAcceptOpts
+--- @return boolean
 function cmp.accept_and_enter(opts)
   return cmp.accept({
     callback = function()
@@ -184,6 +197,7 @@ end
 
 --- Select the first completion item, if there's no selection, accept and feed an enter key to neovim (e.g. to execute the current command in cmdline mode)
 --- @param opts? blink.cmp.CompletionListSelectAndAcceptOpts
+--- @return boolean
 function cmp.select_accept_and_enter(opts)
   return cmp.select_and_accept({
     callback = function()
@@ -195,14 +209,17 @@ end
 
 --- Select the previous completion item
 --- @param opts? blink.cmp.CompletionListSelectOpts
+--- @return boolean
 function cmp.select_prev(opts) return require('blink.cmp.completion.list').select_prev(opts) end
 
 --- Select the next completion item
 --- @param opts? blink.cmp.CompletionListSelectOpts
+--- @return boolean
 function cmp.select_next(opts) return require('blink.cmp.completion.list').select_next(opts) end
 
 --- Inserts the next item (`auto_insert`), cycling to the top of the list if at the bottom, if `completion.list.cycle.from_bottom == true`.
 --- This will trigger completions if none are available, unlike `select_next` which would fallback to the next keymap in this case.
+--- @return boolean
 function cmp.insert_next()
   if not cmp.is_active() then return cmp.show_and_insert() end
 
@@ -211,6 +228,7 @@ end
 
 --- Inserts the previous item (`auto_insert`), cycling to the bottom of the list if at the top, if `completion.list.cycle.from_top == true`.
 --- This will trigger completions if none are available, unlike `select_prev` which would fallback to the next keymap in this case.
+--- @return boolean
 function cmp.insert_prev()
   if not cmp.is_active() then return cmp.show_and_insert() end
 
@@ -234,23 +252,25 @@ function cmp.get_selected_item_idx() return require('blink.cmp.completion.list')
 function cmp.get_items() return require('blink.cmp.completion.list').items end
 
 --- Show the documentation window
+--- @return boolean
 function cmp.show_documentation()
   local menu = require('blink.cmp.completion.windows.menu')
   local documentation = require('blink.cmp.completion.windows.documentation')
-  if documentation.win:is_open() or not menu.win:is_open() then return end
+  if documentation.win:is_open() or not menu.win:is_open() then return false end
 
   local context = require('blink.cmp.completion.list').context
   local item = require('blink.cmp.completion.list').get_selected_item()
-  if not item or not context then return end
+  if not item or not context then return false end
 
   documentation.show_item(context, item)
   return true
 end
 
 --- Hide the documentation window
+--- @return boolean
 function cmp.hide_documentation()
   local documentation = require('blink.cmp.completion.windows.documentation')
-  if not documentation.win:is_open() then return end
+  if not documentation.win:is_open() then return false end
 
   documentation.close()
   return true
@@ -258,18 +278,20 @@ end
 
 --- Scroll the documentation window up
 --- @param count? number
+--- @return boolean
 function cmp.scroll_documentation_up(count)
   local documentation = require('blink.cmp.completion.windows.documentation')
-  if not documentation.win:is_open() then return end
+  if not documentation.win:is_open() then return false end
 
   return documentation.scroll_up(count or 4)
 end
 
 --- Scroll the documentation window down
 --- @param count? number
+--- @return boolean
 function cmp.scroll_documentation_down(count)
   local documentation = require('blink.cmp.completion.windows.documentation')
-  if not documentation.win:is_open() then return end
+  if not documentation.win:is_open() then return false end
 
   return documentation.scroll_down(count or 4)
 end
@@ -279,44 +301,52 @@ end
 function cmp.is_signature_visible() return require('blink.cmp.signature.window').win:is_open() end
 
 --- Show the signature help window
+--- @return boolean
 function cmp.show_signature()
   local config = require('blink.cmp.config').signature
-  if not config.enabled or cmp.is_signature_visible() then return end
+  if not config.enabled or cmp.is_signature_visible() then return false end
+
   require('blink.cmp.signature.trigger').show({ force = true })
   return true
 end
 
 --- Hide the signature help window
+--- @return boolean
 function cmp.hide_signature()
   local config = require('blink.cmp.config').signature
-  if not config.enabled or not cmp.is_signature_visible() then return end
+  if not config.enabled or not cmp.is_signature_visible() then return false end
+
   require('blink.cmp.signature.trigger').hide()
   return true
 end
 
 --- Scroll the signature window up
 --- @param count? number
+--- @return boolean
 function cmp.scroll_signature_up(count)
-  local sig = require('blink.cmp.signature.window')
-  if not sig.win:is_open() then return end
+  local signature = require('blink.cmp.signature.window')
+  if not signature.win:is_open() then return false end
 
-  return sig.scroll_up(count or 4)
+  return signature.scroll_up(count or 4)
 end
 
 --- Scroll the signature window down
 --- @param count? number
+--- @return boolean
 function cmp.scroll_signature_down(count)
-  local sig = require('blink.cmp.signature.window')
-  if not sig.win:is_open() then return end
+  local signature = require('blink.cmp.signature.window')
+  if not signature.win:is_open() then return false end
 
-  return sig.scroll_down(count or 4)
+  return signature.scroll_down(count or 4)
 end
 
 --- Check if a snippet is active, optionally filtering by direction
 --- @param filter? { direction?: number }
+--- @return boolean
 function cmp.snippet_active(filter) return require('blink.cmp.config').snippets.active(filter) end
 
 --- Move the cursor forward to the next snippet placeholder
+--- @return boolean
 function cmp.snippet_forward()
   local snippets = require('blink.cmp.config').snippets
   if not snippets.active({ direction = 1 }) then return false end
@@ -325,6 +355,7 @@ function cmp.snippet_forward()
 end
 
 --- Move the cursor backward to the previous snippet placeholder
+--- @return boolean
 function cmp.snippet_backward()
   local snippets = require('blink.cmp.config').snippets
   if not snippets.active({ direction = -1 }) then return false end
@@ -333,10 +364,7 @@ function cmp.snippet_backward()
 end
 
 --- Ensures that blink.cmp will be notified last when a user adds a character
-function cmp.resubscribe()
-  local trigger = require('blink.cmp.completion.trigger')
-  trigger.resubscribe()
-end
+function cmp.resubscribe() require('blink.cmp.completion.trigger').resubscribe() end
 
 --- Tells the sources to reload a specific provider or all providers (when nil)
 --- @param provider? string
@@ -345,6 +373,7 @@ function cmp.reload(provider) require('blink.cmp.sources.lib').reload(provider) 
 --- Gets the capabilities to pass to the LSP client
 --- @param override? lsp.ClientCapabilities Overrides blink.cmp's default capabilities
 --- @param include_nvim_defaults? boolean Whether to include nvim's default capabilities
+--- @return lsp.ClientCapabilities
 function cmp.get_lsp_capabilities(override, include_nvim_defaults)
   return require('blink.cmp.sources.lib').get_lsp_capabilities(override, include_nvim_defaults)
 end
